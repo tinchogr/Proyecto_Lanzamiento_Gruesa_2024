679319d32482b23f556ce98d84576a01f8e105e2
tinchogroppo <tinchogroppo@gmail.com>
2026-04-14 15:55:25 +0000

    probando pushear

--- Demo_Git_GitHub/Lista.py
+++ Demo_Git_GitHub/Lista.py
@@ -2,5 +2,5 @@
 
 #Crear una lista para ver los cambios entre usuarios
 
-lista_cambios = ["sorgo", "soja", "maiz", "cebada", "trigo", "girasol", "algodon", "carinata", "avena", "vicia", "poroto", "tincho", "mani"]
+lista_cambios = ["sorgo", "soja", "maiz", "cebada", "trigo", "girasol", "algodon", "carinata", "avena", "vicia", "poroto", "tincho", "mani", "prueba nueva" ]
 print (lista_cambios)
